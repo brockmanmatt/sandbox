298ac60e751d7165875a6d8f964cd557f08fd6c4
deggertsen <david@latitude.io>
2020-08-03 17:47:03 -0600

    Board

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Square = () => {
+const Square = i => {
   return (
     <button className="square">
       {/* TODO */}
     </button>
+  );
+}
+
+const Board = i => {
+  const status = 'Next player: X';
+
+  return (
+    <View>
+      <View className="status">{status}</View>
+      <View className="board-row">
+        <Square id='0'></Square>
+        <Square id='1'></Square>
+        <Square id='2'></Square>
+      </View>
+      <View className="board-row">
+        <Square id='3'></Square>
+        <Square id='4'></Square>
+        <Square id='5'></Square>
+      </View>
+      <View className="board-row">
+        <Square id='6'></Square>
+        <Square id='7'></Square>
+        <Square id='8'></Square>
+      </View>
+    </View>
   );
 }
 
