74fe297ca7c31f9dc9d6abef93ac1db1a14e8e99
deggertsen <david@latitude.io>
2020-08-03 15:21:09 -0600

    Add tab navigation to life and tictactoe

--- App.js
+++ App.js
@@ -4,12 +4,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+function TicTacToeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Tic Tac Toe!</Text>
+    </View>
+  );
+}
+
+function LifeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Life!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="TicTacToe" component={TicTacToeScreen} />
+        <Tab.Screen name="Life" component={LifeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
