044c01bc84ebad6135e44cde44cfbe9dc08c5e9e
Dalinar Kholin <george.edward.harwood@gmail.com>
2020-08-04 14:58:42 -0400

    adds Tic Tac Toe Box

---
+++ TicTacToe/Box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Box = ({boxState}) => {
+    return (
+      <View>
+        {boxState}
+      </View>
+    )
+}
+
+export default Box
+
