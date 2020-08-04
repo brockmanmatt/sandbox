7b50e6b6b6019e8a2b24b1949ac2621f7e531d74
Dalinar Kholin <george.edward.harwood@gmail.com>
2020-08-04 15:02:49 -0400

    something is showing up on board

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,12 +5,21 @@ const TicTacToe = () => {
 
   const gameState = React.useState([[0,0,0], [0,0,0], [0,0,0]]);
 
+  const displayBoard = () => {
+    return gameState.map((row) => {
+      return <View>{row[0]}</View>
+    })
+  }
+
     return (
       <View>
         <Text>Open up TicTacToe.js to start working on your game!</Text>
+        {displayBoard()}
       </View>
     )
 }
+
+
 
 export default TicTacToe
 
