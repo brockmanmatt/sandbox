69535d3ac7be1629397220629334de3e5b4d873f
Dalinar Kholin <george.edward.harwood@gmail.com>
2020-08-04 14:55:43 -0400

    sets up intitial state of tic tac toe board

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const TicTacToe = () => {
+
+  const gameState = React.useState([[0,0,0], [0,0,0], [0,0,0]]);
+
     return (
       <View>
         <Text>Open up TicTacToe.js to start working on your game!</Text>
