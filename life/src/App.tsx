76f2bec152a9eaac486a59f4e35b0b4797ceaa45
Dalinar Kholin <george.edward.harwood@gmail.com>
2020-08-05 09:31:49 -0400

    starts displaying grid state

--- life/src/App.tsx
+++ life/src/App.tsx
@@ -4,28 +4,20 @@ import './App.css';
 
 import initialState from "./data/initialState";
 import lifeReducer from "./data/reducer";
-import { LifeState } from './types/LifeState';
 
 function App() {
 
   const [ lifeState, dispatch ] = useReducer(lifeReducer, initialState)
 
+  const displayLife = () => {
+    return lifeState.grid.map((x) => {
+      return <div>here</div>
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {displayLife()}
     </div>
   );
 }
