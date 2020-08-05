8f2405dea3d0cd5e39bb69e02070004570e0b662
Dalinar Kholin <george.edward.harwood@gmail.com>
2020-08-05 09:36:48 -0400

    adds eslint before I go crazy :)

--- life/src/App.tsx
+++ life/src/App.tsx
@@ -1,25 +1,19 @@
-import React, { useReducer } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
 
 import initialState from "./data/initialState";
 import lifeReducer from "./data/reducer";
 
 function App() {
-
-  const [ lifeState, dispatch ] = useReducer(lifeReducer, initialState)
+  const [lifeState, dispatch] = useReducer(lifeReducer, initialState);
 
   const displayLife = () => {
-    return lifeState.grid.map((x) => {
-      return <div>here</div>
+    return lifeState.grid.map(x => {
+      return <div>here</div>;
     });
-  }
+  };
 
-  return (
-    <div className="App">
-      {displayLife()}
-    </div>
-  );
+  return <div className="App">{displayLife()}</div>;
 }
 
 export default App;
