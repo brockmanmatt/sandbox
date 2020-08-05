161778292aab6379cfde09573ce8f88694ee4143
Dalinar Kholin <george.edward.harwood@gmail.com>
2020-08-05 10:55:18 -0400

    adds some additional types and begins intitial implementation of logic

--- life/src/data/rules.ts
+++ life/src/data/rules.ts
@@ -1,9 +1,16 @@
+import { CellNeighbors } from "../types/CellNeighbors";
+
 export default function rules(grid: boolean[][]) {
-  const newState = grid.map((gridRow: boolean[]) => {
-    return gridRow.map((cell: boolean) => {
-      return handleCellState(cell);
+  const newState = grid.map((row: boolean[], x: number) => {
+    return row.map((cell: boolean, y: number) => {
+      const surroundings: CellNeighbors = {
+        alive: getAliveNeighborsCount({ x, y }, grid),
+      };
+      return handleCellState(cell, surroundings);
     });
   });
 }
 
-function handleCellState(cell: boolean) {}
+function getAliveNeighborsCount(cellLocation: Location, grid: boolean[][]): number {}
+
+function handleCellState(cell: boolean, surroundings): boolean {}
---
+++ life/src/types/CellNeighbors.ts
@@ -0,0 +1,4 @@
+export type CellNeighbors = {
+  alive: number;
+  dead: number;
+};
