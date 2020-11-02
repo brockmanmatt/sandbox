f9f3397b879a2bd08a8a9078f6c16813e831c83a
Matt Brockman <brockman@email.arizona.edu>
2020-11-02 12:20:38 -0800

    Added Pseudocode

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+
+/*
+* Visually represent the grid
+1) 3x3 grid, buttons for that
+ - Make buttons
+ - store them with locations
+
+* Allow at least two players to play the game
+ - A, B state
+
+* Implement Win conditions
+- 3 consecutive (array of potential wins)
+
+* Implement Draw conditions
+- no more moves
+
+* Allow users to clear the board
+- easy eenough
+*/
+
+const Board = () => {
+  //This'll have my squares
+  
+}
+
 const TicTacToe = () => {
     return (
       <View>
-        <Text>Open up TicTacToe.js to start working on your game!</Text>
+        <Text>Open up TicTacToe.js to start working on your game! 2</Text>
       </View>
     )
 }
